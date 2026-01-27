2fe753ca5398475c9bcdf111eb0e7c72d6dcd584
Vinit <chaudhari7876@gmail.com>
2026-01-27 12:31:24 +0530

    changes
    logo-changes

--- project-14-investment-calculator/src/Components/Header.jsx
+++ project-14-investment-calculator/src/Components/Header.jsx
@@ -1,8 +1,8 @@
-import logo from "../assets/investment-calculator-logo.png"
+import logo from "../assets/rupee-logo.png"
 
 export default function Header(){
     return <header className="flex flex-col items-center pt-6">
-        <img src={logo} alt="logo" className="h-32"/>
+        <img src={logo} alt="logo" className="h-32 rounded-full object-cover"/>
         <h1 className="text-white text-3xl">Investment Calculator</h1>
     </header>
 }
