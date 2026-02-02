dd0a6c871173a485c31063d199a5cda0b041c936
Vinit <chaudhari7876@gmail.com>
2026-02-02 14:36:45 +0530

    completed the challanges
    completed the challenges page

---
+++ project-16-refs-portals/src/App.jsx
@@ -0,0 +1,17 @@
+import Player from './components/Player.jsx';
+import TimerChallange from './components/TimerChallange.jsx';
+function App() {
+  return (
+    <>
+      <Player />
+      <div id="challenges">
+        <TimerChallange title="Easy" targetTime={1}/>
+        <TimerChallange title="Not Easy" targetTime={5}/>
+        <TimerChallange title="Getting Touch" targetTime={10}/>
+        <TimerChallange title="Pros Only" targetTime={15}/>
+      </div>
+    </>
+  );
+}
+
+export default App;
