5703132f12b40103ecd38d11f1724da830c67b3e
Vinit <chaudhari7876@gmail.com>
2026-02-03 17:39:53 +0530

    fixed the issue
    fixed the issue where the page was not showing because of not writing the refs into the component function props

--- project-18-practice-project/src/App.jsx
+++ project-18-practice-project/src/App.jsx
@@ -1,49 +1,50 @@
-import ProjectSidebar from "./components/ProjectSidebar"
-import NewProject from "./components/NewProject"
-import NoProjectSelected from "./components/NoProjectSelected"
-import { useState } from "react"
+import ProjectSidebar from "./components/ProjectSidebar";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import { useState } from "react";
 
-export default function App(){
+export default function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
 
-    const [projectsState, setProjectsState] = useState({
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  function handleAddProject(projectData) {
+    const projectId= Math.random()
+    const newProject = {
+      ...projectData,
+      id: Math.random(),
+    };
+    setProjectsState((prevState) => {
+      return { ...prevState,
         selectedProjectId: undefined,
-        projects: []
-    })
+        projects: [...prevState.projects, newProject] };
+    });
+  }
 
-    function handleStartAddProject(){
-        setProjectsState(prevState => {
-            return{
-                ...prevState,
-                selectedProjectId: null,
-            }
-        });
-    }
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
 
-    let content;
+  console.log(projectsState);
 
-    console.log(projects)
-    
-    if(projectsState.selectedProjectId === null){
-        content = <NewProject onAdd={handleAddProject}/>
-    }else if (projectsState.selectedProjectId === undefined){
-        content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
-    }
-
-    function handleAddProject(projectData){
-
-        const newProject ={
-           ...projectData,
-           id: Math.random()
-        }
-        setProjectsState(prevState => {
-            return {...prevState, projects: [...prevState.projects, newProject]}
-        })
-    }
-
-    return(
-        <main className="h-screen my-8 flex gap-8">
-            <ProjectSidebar onStartAddProject={handleStartAddProject}/>
-            {content}
-        </main>
-    )
+  return (
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects}/>
+      {content}
+    </main>
+  );
 }
