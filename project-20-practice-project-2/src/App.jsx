d84d3869010d210be4fe46159d1dfb9c84e02c27
Vinit <chaudhari7876@gmail.com>
2026-02-10 16:46:01 +0530

    issue fixed
    small issue which was big, fixed.

--- project-20-practice-project-2/src/App.jsx
+++ project-20-practice-project-2/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header.jsx";
 import AddPerson from "./components/AddPerson.jsx";
 import { useState } from "react";
+import Button from "./components/Button.jsx";
 
 export default function App() {
   const [persons, setPersons] = useState([]);
@@ -11,6 +12,14 @@ export default function App() {
       name: name,
     };
     setPersons((prevPersons) => [...prevPersons, newPerson]);
+  }
+
+  function handleRemovePerson(id){
+    //okay so we are not actually deleting the person from the existing array
+    //but instead we are simply using filter method which automatically creates a new array for us
+    //we are using filter method inside setter function pair so it will set the new values to the array without that person which is to be 
+    //removed, that is the efficient way to handle removing array
+    setPersons((prevPersons)=> prevPersons.filter((person) => person.id != id))
   }
 
   return (
@@ -27,7 +36,7 @@ export default function App() {
             <ul className="space-y-2">
               {persons.map((person) => (
                 <li key={person.id} className="bg-stone-800 p-3 rounded">
-                  {person.name}
+                  <div className="flex gap-2 items-center"><span>{person.name}</span><Button onClick={() => handleRemovePerson(person.id)}>Remove</Button></div>
                 </li>
               ))}
             </ul>
