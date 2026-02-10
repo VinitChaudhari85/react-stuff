335b064882d7fd8f8bc25bad1e2f9cb886e46599
Vinit <chaudhari7876@gmail.com>
2026-02-10 15:06:46 +0530

    practicing
    practicing a new project

---
+++ project-20-practice-project-2/src/App.jsx
@@ -0,0 +1,6 @@
+import Header from "./components/Header"
+export default function App() {
+  return <div className="min-h-screen bg-stone-900 text-white pt-6">
+    <Header heading="Expense Splitter"/>
+  </div>
+};
---
+++ project-20-practice-project-2/src/components/Header.jsx
@@ -0,0 +1,5 @@
+export default function Header({heading}) {
+    return <header className="text-center text-2xl font-bold">
+        <h1>{heading}</h1>
+    </header>
+};
